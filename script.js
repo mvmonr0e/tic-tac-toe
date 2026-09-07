53bf5fe756539f375f2ecfabbd95aa26a3c1b2b9
michael <mvmnr01@gmail.com>
2026-09-07 10:16:53 -0500

    skeleton

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const Gameboard = {
+    // what if in the board we tracked if each row and column was already filled,
+    // then only check unfilled rows, columns, and diagonals on each gameOver() call?
     board: [
         [0,0,0],
         [0,0,0],
@@ -7,27 +9,42 @@ const Gameboard = {
 }
 canMove = false;
 
+// player object
 class Player {
-    constructor (name, tic, move){
-        this.name = name
-        this.tic = tic
-        this.move = move
+    constructor (name, tic, move, winner){
+        this.name = name;
+        this.tic = tic;
+        this.move = move;
+        this.winner = winner;
     }
 }
 
+//create new player obj
 function createPlayer(name, tic){
-    canMove = !canMove;
-    return new Player(name,tic,canMove);
+
 }
 
-function move(player, row, col){
-    if (!isValid(row,col)){
-        console.log('move is not valid, try again')
-    }
+
+// move on the board
+function move(player,row, col){
+
 }
 
+// check if game is over
+function gameOver(){
+
+}
+
+
+// check if move is valid
 function isValid(row,col){
+
+}
+
+//play game
+function play(){
     
 }
+
 const p1 = createPlayer('p1','x');
 const p2 = createPlayer('p2','o');
